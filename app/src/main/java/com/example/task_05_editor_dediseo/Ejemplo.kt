56a27df626e301fccbf213bf6b14ee1429533961
Tommy <tomsan@alu.centroafuera.es>
2022-11-04 14:19:27 +0100

    Commit(04/11/2022)

---
+++ app/src/main/java/com/example/task_05_editor_dediseo/Ejemplo.kt
@@ -0,0 +1,11 @@
+package com.example.task_05_editor_dediseo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ejemplo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ejemplo)
+    }
+}
